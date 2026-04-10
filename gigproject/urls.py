ea63d659b260b6d9b76d25b5eaffbf8f08c4c8ce
Jack Gardner <jtgardner99@gmail.com>
2026-04-10 18:05:33 -0500

    initial commit

--- gigproject/urls.py
+++ gigproject/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from gigapi.views import ClientViewSet
 
 router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'clients', ClientViewSet, 'client')
 
 urlpatterns = [
     path('', include(router.urls)),
