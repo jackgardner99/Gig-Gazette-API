f6bca164f6b6b08ed39e1aa5e0711762415dee1a
Jack Gardner <jtgardner99@gmail.com>
2026-05-17 12:02:15 -0500

    purchasing ticket links

--- gigapi/models/show.py
+++ gigapi/models/show.py
@@ -7,6 +7,7 @@ class Show(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="shows", null=True, blank=True)
     event_title = models.CharField(max_length=255)
     poster_img = models.ImageField(upload_to="posters/", blank=True, null=True)
+    ticket_link = models.URLField(blank=True, null=True)
     date = models.DateField()
     start_time = models.TimeField()
     end_time = models.TimeField()
